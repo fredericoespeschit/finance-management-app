3f2135e5c154a440af9669a6b3b9ee40ac5d87b2
fredericoespeschit <fespeschitt@gmail.com>
2023-07-14 22:37:39 -0300

    cálculo de contagem de despesas e receitas com base em na lista filtrada de itens.

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,37 @@ import { items } from './data/items';
 import { getCurrentMonth, filterListByMonth } from './helpers/dateFilter';
 import { TableArea } from './components/TableArea';
 import { InfoArea } from './components/InfoArea';
+import { categories } from './data/categories';
 
 const App = () => {
   const [list, setList] = useState(items); // eslint-disable-line @typescript-eslint/no-unused-vars
   const [filteredList, setFilteredList] = useState<Item[]>([]);
-  const [currentMonth, setCurrentMonth] = useState(getCurrentMonth()); // eslint-disable-line @typescript-eslint/no-unused-vars
-  const [income, setIncome] = useState(0); // eslint-disable-line @typescript-eslint/no-unused-vars
-  const [expense, setExpense] = useState(0); // eslint-disable-line @typescript-eslint/no-unused-vars
+  const [currentMonth, setCurrentMonth] = useState(getCurrentMonth()); 
+  const [income, setIncome] = useState(0); 
+  const [expense, setExpense] = useState(0);
 
 
   useEffect(()=>{
     setFilteredList( filterListByMonth(list, currentMonth) );
   }, [list, currentMonth]);
 
+
+  useEffect(()=>{
+    let incomeCount = 0;
+    let expenseCount = 0;
+
+    for (let i in filteredList){
+      if(categories[filteredList[i].category].expense){
+        expenseCount += filteredList[i].value;
+      } else {
+        incomeCount += filteredList[i].value;
+      }
+    }
+    
+    setIncome(incomeCount);
+    setExpense(expenseCount);
+
+  }, [filteredList]);
 
   const handleMonthChange = (newMonth: string) => {
     setCurrentMonth(newMonth);
